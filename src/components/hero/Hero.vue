53938bf778421a7579b96fb6d6bf499a2aa23a7e
Sumukha210 <sumukhakb@gmail.com>
2021-07-01 01:39:37 +0530

    hero and navbar is completed

---
+++ src/components/hero/Hero.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <div class="left">
+      <div class="content">
+        <h1 class="heading">
+          Virtual healthcare <br />
+          for you
+        </h1>
+        <p>
+          Trafalgar provides progressive, and affordable healthcare,
+          <br />accessible on mobile and online for <br />
+          everyone
+        </p>
+        <button class="btn btn-primary">Consult today</button>
+      </div>
+    </div>
+
+    <div class="right">
+      <img src="/images/hero.png" alt="hero image" />
+    </div>
+
+    <div class="dots">
+      <img src="/images/dots.png" alt="" />
+    </div>
+  </div>
+</template>
+
+
+<style scoped lang="scss" src="./Hero.scss"></style>
---
+++ src/components/navbar/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <nav>
+    <div class="container">
+      <div class="logo">
+        <img src="/images/logo.png" alt="logo" />
+      </div>
+
+      <ul :class="`menu ${!isOpen && 'open'}`">
+        <li><a href="#" class="active">Home</a></li>
+        <li><a href="#">Find a doctor</a></li>
+        <li><a href="#">Apps</a></li>
+        <li><a href="#">Tesimonials</a></li>
+        <li><a href="#">About us</a></li>
+      </ul>
+
+      <div class="mobileMenu" @click="handleMenu()">
+        <i :class="isOpen ? 'fas fa-bars' : 'fas fa-times'"></i>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isOpen: true,
+    };
+  },
+
+  methods: {
+    handleMenu() {
+      this.isOpen = !this.isOpen;
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss" src="./Navbar.scss"></style>
